0b38aa8379f5c331bdc959f92d6c192dcb63faff
zzz3bra <z3vs@yandex.ru>
2015-05-22 21:05:25 +0300

    test2

---
+++ !CourseKG(WPF)/Light.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _CourseKG_WPF_
+{
+    class Light
+    {
+        #region Public Fields
+        public Color StraightLineColor { get; private set; }
+        public Color CurveLineColor { get; private set; }
+
+        #endregion
+    }
+}
